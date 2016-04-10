e941ee1514c4574d2a21143e423acc1c7b8e3c8c
Argeadi <jovica.mircevski@gmail.com>
2016-04-10 21:04:17 +0000

    2/4

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -43,6 +43,17 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
         sporocilo = 'Neznan ukaz';
       }
       break;
+    case 'dregljaj':
+      besede.shift();
+      var del = besede.join(' ');
+
+      if(del){
+        sporocilo = "Dregljaj za " + del;
+        this.socket.emit('dregljaj', { vzdevek: del});
+      }else{
+        sporocilo = 'Neznan ukaz.';
+      }
+      break;
     default:
       sporocilo = 'Neznan ukaz.';
       break;
